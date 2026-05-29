f279ae26daac2ca3f7dd7df69b9b4cd2ded4ec8e
Daniel <suckerpunch1@hotmail.com>
2026-05-29 12:58:22 -0500

    convert files to routing skeletons

---
+++ app/api/jobs/route.ts
@@ -0,0 +1,17 @@
+import {NextResponse} from 'next/server';
+
+export async function GET() {
+
+}
+
+export async function POST() {
+
+}
+
+export async function PUT() {
+
+}
+
+export async function DELETE() {
+
+}
